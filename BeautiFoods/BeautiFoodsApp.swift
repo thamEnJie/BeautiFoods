5a72a5ae8a3531c4fb3c625dcc9294937f5bda67
thamEnJie <tham_en_jie@s2021.ssts.edu.sg>
2022-08-23 10:27:55 +0800

    Initial Commit

---
+++ BeautiFoods/BeautiFoodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeautiFoodsApp.swift
+//  BeautiFoods
+//
+//  Created by Tham En Jie on 23/8/22.
+//
+
+import SwiftUI
+
+@main
+struct BeautiFoodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeautiFoods/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BeautiFoods
+//
+//  Created by Tham En Jie on 23/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
