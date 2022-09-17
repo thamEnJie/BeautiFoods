0d2fa5ca581afece19d0cb280df4d59c1c12c8f9
thamEnJie <tham_en_jie@s2021.ssts.edu.sg>
2022-09-17 23:35:31 +0800

    Moved Badge extension to HomeContentView

--- BeautiFoods/HomeContentView.swift
+++ BeautiFoods/HomeContentView.swift
@@ -161,6 +161,26 @@ struct HomeContentView: View {
     }
 }
 
+extension View {
+    func badge(width: Int = 24, count: Int = 10, tintColour: Color = .red, offset: Int = 0) -> some View {
+        ZStack(alignment: .topTrailing) {
+            self
+            ZStack {
+                if count != 0 {
+                    Text("\(count)")
+                        .font(.system(size: CGFloat(width/20*13)))
+                        .fontWeight(.bold)
+                        .frame(width: CGFloat(width), height: CGFloat(width))
+                        .foregroundColor(Color(UIColor.systemBackground))
+                        .background(Circle().fill(tintColour))
+                        .transition(.scale)
+                }
+            }
+            .offset(x: CGFloat(width/2+offset), y: CGFloat(width/2*(-1)-offset))
+        }
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         HomeContentView()
